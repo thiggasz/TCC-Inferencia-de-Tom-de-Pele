1165154a03e3fee8224fd569cfaa7f18ccb0826c
Thiago <thiagozduarte@gmail.com>
2026-03-28 16:57:42 -0300

    Face detection and Face segmentation classes

---
+++ src/pipeline/face_detection.py
@@ -0,0 +1,135 @@
+import cv2
+import numpy as np
+import mediapipe as mp
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+class FacePipeline:
+    def __init__(self):
+        # Initiliaze base face detector
+        self.mp_face_detection = mp.solutions.face_detection
+        self.detector_close = self.mp_face_detection.FaceDetection(model_selection=0, min_detection_confidence=0.4)
+        self.detector_far = self.mp_face_detection.FaceDetection(model_selection=1, min_detection_confidence=0.4)
+        
+        # Initialize face detector based on face mesh
+        self.mp_face_mesh = mp.solutions.face_mesh
+        self.face_mesh = self.mp_face_mesh.FaceMesh(static_image_mode=True, max_num_faces=1, min_detection_confidence=0.5)
+
+    def get_face_crop(self, image: np.array) -> np.array:
+        """Find a face in the image and extracts a padded crop"""
+        h, w, _ = image.shape
+        image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        
+        # Detects face
+        results = self.detector_close.process(image_rgb)
+        if not results.detections:
+            results = self.detector_far.process(image_rgb)
+            
+        if not results.detections:
+            return None
+            
+        detection = max(results.detections, key=lambda det: det.score[0])
+        
+        # Obtain the padded face bounding box
+        bbox = detection.location_data.relative_bounding_box
+        
+        xmin, ymin = int(bbox.xmin * w), int(bbox.ymin * h)
+        width, height = int(bbox.width * w), int(bbox.height * h)
+        
+        pad_x, pad_y = int(width * 0.5), int(height * 0.5)
+        
+        x1, y1 = max(0, xmin - pad_x), max(0, ymin - pad_y)
+        x2, y2 = min(w, xmin + width + pad_x), min(h, ymin + height + pad_y)
+        
+        face_crop = image[y1:y2, x1:x2]
+        if face_crop.size == 0: 
+            return None
+            
+        return face_crop
+
+    def align_face(self, face_crop: np.array):
+        """Detect face landmarks and uses then to rotate and align the face"""
+        crop_rgb = cv2.cvtColor(face_crop, cv2.COLOR_BGR2RGB)
+        mesh_results = self.face_mesh.process(crop_rgb)
+        
+        if not mesh_results.multi_face_landmarks:
+            return None, None, None
+            
+        # Find landmarks    
+        landmarks = mesh_results.multi_face_landmarks[0].landmark
+        ch, cw, _ = face_crop.shape
+        
+        left_eye_x, left_eye_y = int(landmarks[33].x * cw), int(landmarks[33].y * ch)
+        right_eye_x, right_eye_y = int(landmarks[263].x * cw), int(landmarks[263].y * ch)
+        
+        eye_center = ((left_eye_x + right_eye_x) // 2, (left_eye_y + right_eye_y) // 2)
+        angle = np.degrees(np.arctan2(right_eye_y - left_eye_y, right_eye_x - left_eye_x))
+        
+        # Rotate the iamge to align the eyes in the face bouding box
+        M = cv2.getRotationMatrix2D(eye_center, angle, 1.0)
+        aligned_crop = cv2.warpAffine(face_crop, M, (cw, ch), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+        
+        return aligned_crop, landmarks, M
+
+    def get_final_crop(self, aligned_crop: np.array, landmarks, M) -> np.array:
+        """Uses the ladmarks to rotate the image and obtain a 1:1 bounding box"""
+        ch, cw, _ = aligned_crop.shape
+        
+        # Applies the rotation matrix in the landmarks
+        pts = np.array([[[lm.x * cw, lm.y * ch]] for lm in landmarks], dtype=np.float32)
+        transformed_pts = cv2.transform(pts, M).squeeze()
+        
+        # Find the face limits
+        min_x, min_y = np.min(transformed_pts, axis=0)
+        max_x, max_y = np.max(transformed_pts, axis=0)
+        
+        center_x, center_y = (min_x + max_x) / 2, (min_y + max_y) / 2
+        box_w, box_h = max_x - min_x, max_y - min_y
+        
+        # Creates a bounding box with 40% padding
+        max_side = max(box_w, box_h)
+        side_with_pad = int(max_side * 1.4)
+        half_side = side_with_pad // 2
+        
+        fx1, fy1 = int(center_x - half_side), int(center_y - half_side)
+        fx2, fy2 = int(center_x + half_side), int(center_y + half_side)
+        
+        # See if the iamge its completly contained in the bounding box
+        pad_left, pad_top = max(0, -fx1), max(0, -fy1)
+        pad_right, pad_bottom = max(0, fx2 - cw), max(0, fy2 - ch)
+        
+        safe_fx1, safe_fy1 = max(0, fx1), max(0, fy1)
+        safe_fx2, safe_fy2 = min(cw, fx2), min(ch, fy2)
+        
+        # Get the face crop
+        face_crop = aligned_crop[safe_fy1:safe_fy2, safe_fx1:safe_fx2]
+        
+        if any(p > 0 for p in (pad_top, pad_bottom, pad_left, pad_right)):
+            final_face = cv2.copyMakeBorder(face_crop, pad_top, pad_bottom, pad_left, pad_right, cv2.BORDER_CONSTANT, value=[0, 0, 0])
+        else:
+            final_face = face_crop
+            
+        return final_face
+
+    def process_image(self, image: np.array) -> np.array:
+        """Pipeline for face detection, alignment and crop extraction"""
+        try:
+            face_crop = self.get_face_crop(image)
+            if face_crop is None: 
+                return None
+            
+            aligned_crop, landmarks, M = self.align_face(face_crop)
+            if aligned_crop is None: 
+                return None
+            
+            final_face = self.get_final_crop(aligned_crop, landmarks, M)
+            
+            return final_face
+            
+        except cv2.error as e:
+            logging.error(f"OpenCv processing error: {e}")
+            return None
+        except Exception as e:
+            logging.error(f"Unexpected error in FacePipeline: {e}")
+            return None
---
+++ src/pipeline/face_segmentation.py
@@ -0,0 +1,46 @@
+import torch
+from libs.FaceParser.inference import Segmentator
+from typing import Tuple, List, Generator
+from tqdm import tqdm
+import numpy as np
+
+"""""
+
+Given a imagem crop of a detected face, segmentates the face in the image, excluding the protected atributes,
+resulting in a skin pixels mask
+
+""""" 
+
+class FaceSegmenter:
+    def __init__(self, batch_size: int = 32):
+        self.segmentator = Segmentator()
+        
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.segmentator.model.to(self.device)
+        
+        if self.device.type == 'cuda':
+            gpu_name = torch.cuda.get_device_name(0)
+            print(f"GPU detectada: Usando {gpu_name} para segmentação.")
+        else:
+            print("Aviso: Nenhuma GPU detectada. Usando CPU para segmentação (será mais lento).")
+            
+        self.batch_size = batch_size
+
+    def deep_segmentation(self, image: np.array) -> Tuple[np.array, np.array]:
+        # Parâmetro inútil removido
+        return self.segmentator.inference(image)
+
+    def batch_list(self, data: List[np.array]) -> List[List[np.array]]:
+        return [data[i:i + self.batch_size] for i in range(0, len(data), self.batch_size)]
+
+    def batched_deep_segmentation(self, images: List[np.array]) -> Tuple[List[np.array], List[np.array]]:
+        batches = self.batch_list(images)
+        masks = []
+        skins = []
+        
+        for batch in tqdm(batches, desc="Segmentando batches"):
+            m, sk = self.segmentator.batched_inference(batch)
+            masks.extend(m)
+            skins.extend(sk)
+            
+        return masks, skins
